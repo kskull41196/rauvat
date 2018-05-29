a22fef132f3188fb53d5f1e460606aa5a96cad06
Ly Thanh Liem <liembsbs123@gmail.com>
2018-05-29 17:33:37 +0700

    update table

---
+++ migrations/20180529092556-v_1_0_0_drop_tbl_paid_history.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    return await queryInterface.dropTable('tbl_paid_history');
+  },
+  down: async function (queryInterface, Sequelize) {
+    return await queryInterface.createTable('tbl_paid_history', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV1,
+        primaryKey: true
+      },
+      bill_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'tbl_bill',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      pay_amount: {
+        type: Sequelize.BIGINT,
+        validate: {
+          min:0,
+        },
+        defaultValue:0,
+        allowNull: false
+      },
+      retain_amount: {
+        type: Sequelize.BIGINT,
+        validate: {
+          min:0,
+        },
+        defaultValue:0,
+        allowNull: false
+      },
+      payment_method: {
+        type: Sequelize.ENUM,
+        values: ['COD', 'ONLINE','WALLET'],
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
+      },
+      created_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+      },
+      updated_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+      },
+      deleted_at: { type: 'TIMESTAMP' }
+    })
+  }
+};
