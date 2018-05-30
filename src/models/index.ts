f09af517e9f9435ca358954065bd66731f5909e4
Ly Thanh Liem <liembsbs123@gmail.com>
2018-05-30 10:35:28 +0700

    close mongo

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,4 @@
-export * from './base.mongo'
+//export * from './base.mongo'
 export * from './base.pg'
 //export * from './collections'
 export * from './tables'
--- src/services/crud/sampleCrudService.mongo.ts
+++ src/services/crud/sampleCrudService.mongo.ts
@@ -1,8 +1,8 @@
-import { CrudService } from '../crudService.mongo'
-import { Sample } from '@/models/collections'
-import * as mongoose from "mongoose"
-export class SampleCrudService extends CrudService<typeof Sample> {
-    constructor() {
-        super(Sample)
-    }
-}
+// import { CrudService } from '../crudService.mongo'
+// import { Sample } from '@/models/collections'
+// import * as mongoose from "mongoose"
+// export class SampleCrudService extends CrudService<typeof Sample> {
+//     constructor() {
+//         super(Sample)
+//     }
+// }
