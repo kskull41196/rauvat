355acf29e15de91137cbe1c049b8012d3eb1d350
Ly Thanh Liem <liembsbs123@gmail.com>
2018-07-22 16:21:44 +0700

    create api get followers and followings

--- src/controllers/crud/followingController.ts
+++ src/controllers/crud/followingController.ts
@@ -1,10 +1,64 @@
 import { CrudController } from '../crudController'
-import { ICrudOption, errorService, followingService } from '@/services'
+import { ICrudOption, errorService, followingService, userService } from '@/services'
 
 
 export class FollowingController extends CrudController<typeof followingService> {
     constructor() {
         super(followingService)
+    }
+
+    async getFollowers(params: any) {
+        let {
+            user_id
+        } = params;
+
+        return await userService.getList({
+            filter: {
+
+            },
+            include: [
+                {
+                    association: 'followings',
+                    where: {
+                        user_id: user_id
+                    },
+                    attributes: []
+                }
+            ]
+        });
+    }
+
+    async getFollowings(params: any) {
+        let {
+            user_id
+        } = params;
+
+        return await userService.getList({
+            filter: {
+
+            },
+            include: [
+                {
+                    association: 'followers',
+                    where: {
+                        user_id: user_id
+                    },
+                    attributes: []
+                }
+            ]
+        });
+    }
+
+    async getNewfeeds(params: any) {
+        return params;
+    }
+
+    async followUser(params: any) {
+        return params;
+    }
+
+    async unFollowUser(params: any) {
+        return params;
     }
 
 }
--- src/routers/v1/following.ts
+++ src/routers/v1/following.ts
@@ -9,4 +9,70 @@ export default class Following extends CrudRouter<typeof followingController> {
 
     }
 
+    customRouting() {
+        this.router.get('/followers', this.getFollowersMiddlewares(), this.route(this.getFollowers));
+        this.router.get('/followings', this.getFollowingsMiddlewares(), this.route(this.getFollowings));
+        this.router.get('/newfeeds', this.getNewfeedsMiddlewares(), this.route(this.getNewfeeds));
+        this.router.post('/follow/:user_id', this.followUserMiddlewares(), this.route(this.followUser));
+        this.router.post('/unfollow/:user_id', this.unFollowUserMiddlewares(), this.route(this.unFollowUser));
+    }
+
+    getFollowersMiddlewares(): any[] {
+        return [
+            authInfoMiddleware.run()
+        ]
+    }
+
+    async getFollowers(req: Request, res: Response) {
+        req.body.user_id = req.tokenInfo.payload.user_id;
+        const result = await this.controller.getFollowers(req.body);
+        this.onSuccess(res, result);
+    }
+
+    getFollowingsMiddlewares(): any[] {
+        return [
+            authInfoMiddleware.run()
+        ]
+    }
+
+    async getFollowings(req: Request, res: Response) {
+        req.body.user_id = req.tokenInfo.payload.user_id;
+        const result = await this.controller.getFollowings(req.body);
+        this.onSuccess(res, result);
+    }
+
+    getNewfeedsMiddlewares(): any[] {
+        return [
+
+        ]
+    }
+
+    async getNewfeeds(req: Request, res: Response) {
+        const result = await this.controller.getNewfeeds(req.body);
+        this.onSuccess(res, result);
+    }
+
+    followUserMiddlewares(): any[] {
+        return [
+
+        ]
+    }
+
+    async followUser(req: Request, res: Response) {
+        const result = await this.controller.followUser(req.body);
+        this.onSuccess(res, result);
+    }
+
+    unFollowUserMiddlewares(): any[] {
+        return [
+
+        ]
+    }
+
+    async unFollowUser(req: Request, res: Response) {
+        const result = await this.controller.unFollowUser(req.body);
+        this.onSuccess(res, result);
+    }
+
+
 }
